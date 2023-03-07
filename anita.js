856eedb4f7e4ac9e51962967ce713011efc9694f
Masian Parkire <parkiremasian@gmail.com>
2023-03-07 19:01:50 +0300

    made changes

--- anita.js
+++ anita.js
@@ -1,6 +1,9 @@
-let a = "Beautiful";
+let b = "beautiful";
 console.log(b[5],b[6],b[7],b[8]);
-
-String.prototype.insert =function(index,string){
-    if(index>0)
+String.prototype.insert = function(index, string){
+    if (index > 0)
+    {
+        return food.substring(0,index) + string + food.substring(index, food.length);
+    }
+    return string + food
 }
